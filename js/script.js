a4db4c2c6a7091829d1e18e0411971302e23bb8a
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2021-11-02 17:47:01 +0100

    I need to fix the reset game

--- js/script.js
+++ js/script.js
@@ -13,12 +13,16 @@ bntPlayGame.addEventListener('click',function() {
 
   gameContainer.classList.remove('d-none');
 
+
   if (leavelDifficulty.value === '1') {
     console.log('easy');
+    howManySquare(gameContainer,'easy', 49);
   } else if (leavelDifficulty.value === '2') {
     console.log('hard');
+    howManySquare(gameContainer,'hard', 81);
   } else if (leavelDifficulty.value === '3') {
     console.log('crazy');
+    howManySquare(gameContainer,'crazy', 100);
   } else {
     title.classList.remove('d-none');
     gameContainer.classList.add('d-none');
@@ -31,9 +35,10 @@ bntPlayGame.addEventListener('click',function() {
  * Funziona che genera quadrati all'interno del div selezionato
  * @param {div} where 
  */
-function createSquare(elementInHtml) {
+ function createSquare(elementInHtml, className) {
   const square = document.createElement('div');
   square.className = 'square';
+  square.classList.add(className);
   elementInHtml.append(square);
 }
 
@@ -41,10 +46,12 @@ function createSquare(elementInHtml) {
  * Funzione che genera un max numero di quadrati
  * @param {Number} max 
  */
-function howManySquare (elementInHtml, max) {
+function howManySquare (elementInHtml, className, max) {
   for (let i = 0; i < max; i++) {
-    createSquare(elementInHtml);
+    createSquare(elementInHtml, className);
   }
 }
+
+
 
 
